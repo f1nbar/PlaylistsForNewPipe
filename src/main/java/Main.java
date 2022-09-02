0398419459a78bfaf5f5c4498651cd74e535b441
Finbar <finbar@tutanota.com>
2022-09-02 16:06:29 +0100

    Debugging issues that come from db changes and tidying up some code. Needs a rewrite. Added flatlaf for future UI redo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,9 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        Extract ext = new Extract();
-        ArrayList<ArrayList<String>> playlist = ext.connect();
-        ext.ytPlaylistFromLinks(playlist.get(0));
-    }
+  public static void main(String[] args) throws Exception {
+    Extract extract = new Extract();
+    ArrayList<ArrayList<String>> playlist = extract.connect();
+    extract.ytPlaylistFromLinks(playlist.get(0));
+  }
 }
