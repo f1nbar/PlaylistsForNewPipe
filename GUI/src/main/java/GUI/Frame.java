c3499ebab2d28be2f486bdbf4afedfc6c0bb2b4d
Finbar <finbar@tutanota.com>
2022-09-13 12:36:22 +0100

    Add combobox for playlist selection

--- GUI/src/main/java/GUI/Frame.java
+++ GUI/src/main/java/GUI/Frame.java
@@ -1,29 +1,43 @@
 package GUI;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Vector;
+
 import service.Extract;
 
 public class Frame extends JFrame {
 
+  static JButton generateButton, fileBrowseButton;
+  static JLabel headerImage;
+  static JComboBox<String> comboBox;
+
   public Frame() throws Exception {
+
     initUI();
     pack();
   }
 
   private void initUI() throws Exception {
+
     setTitle("Select a NewPipe DB file");
-    setResizable(true);
+    setResizable(false);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    ImageIcon icon = new ImageIcon("icon.png");
+    ImageIcon icon = new ImageIcon("images/icon.png");
     setIconImage(icon.getImage());
     setLayout(new GridBagLayout());
     setSize(400, 800);
     setVisible(true);  
+    Image image =  ImageIO.read(new File("images/icon.png")).getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+    headerImage = new JLabel(new ImageIcon(image));
+    generateButton = new JButton("Generate Playlist");
 
     GridBagConstraints constraints = new GridBagConstraints();
-    JButton fileBrowseButton = new JButton("Select File");
+    fileBrowseButton = new JButton("Select File");
     fileBrowseButton.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e){
@@ -37,14 +51,25 @@ public class Frame extends JFrame {
       }
     });
 
-    JButton generateButton = new JButton("Generate");
+    Vector<String> playlistNames = new Vector<>();
+    playlistNames.add("Waiting for playlist database");
+    DefaultComboBoxModel model = new DefaultComboBoxModel<>(playlistNames);
+
+    comboBox = new JComboBox<>(model);
+
 
     constraints.gridwidth = GridBagConstraints.REMAINDER;
     constraints.fill = GridBagConstraints.HORIZONTAL;
     constraints.insets = new Insets(10,10,10,10);
 
+    add(headerImage, constraints);
     add(fileBrowseButton, constraints);
+    add(comboBox, constraints);
     add(generateButton, constraints);
+  }
+
+  public void fillCombobox(ArrayList<String> playlistNames) {
+
   }
 }
 
