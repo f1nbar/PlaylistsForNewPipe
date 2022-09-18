3230ce54852cbf9b522c65d07a37eb15bf1099bc
Finbar <finbar@tutanota.com>
2022-09-18 11:56:46 +0100

    Playlist generation that handles youtube's max size of 50

--- service/src/main/java/service/Extract.java
+++ service/src/main/java/service/Extract.java
@@ -1,6 +1,6 @@
+package service;
 
 import java.sql.*;
-
 import java.util.ArrayList;
 
 public class Extract {
@@ -66,17 +66,31 @@ public class Extract {
   public void createPlaylist(int uid) throws Exception {
 
     ArrayList<String> videoURLS = queryVideoIDs(uid);
+    System.out.println(videoURLS.size());
     int playlistsToCreate = (videoURLS.size() / 50) + 1;
-    ArrayList<ArrayList <String>> generatedPlaylists = new ArrayList<>();
+    ArrayList<ArrayList <String>> subLists = new ArrayList<>();
+    ArrayList<String> generatedPlaylists = new ArrayList<>();
 
     for (int i = 0; i < playlistsToCreate; i++) { //make sublists
+      if (videoURLS.size() < 50) {
+        subLists.add(new ArrayList<String>(videoURLS.subList(0, videoURLS.size())));
+        videoURLS.subList(0, videoURLS.size()).clear();;
+      } else {
+        subLists.add(new ArrayList<String>(videoURLS.subList(0, 49)));
+        videoURLS.subList(0, 49).clear();;
+      }
+      generatedPlaylists.add(playlistStringBuilder(subLists.get(i)));
+      System.out.println(generatedPlaylists.get(i));
     }
+  }
+
+  public String playlistStringBuilder(ArrayList<String> videoURLS) {
 
     StringBuilder stringBuilder = new StringBuilder();
     for (String url : videoURLS) {
       stringBuilder.append(url + ","); //comma seperate all video urls to append to playlist link
     }
-    String playlist = "https://www.youtube.com/watch_videos?video_ids=" + stringBuilder;
-    System.out.println(playlist);
+    return "https://www.youtube.com/watch_videos?video_ids=" + stringBuilder;
   }
+
 }
