690d85349b1f516c4d90e3b3602df9b88e031c4c
Finbar <finbar@tutanota.com>
2022-09-27 11:50:54 +0100

    Set and get generated playlists

--- service/src/main/java/service/Extract.java
+++ service/src/main/java/service/Extract.java
@@ -5,8 +5,9 @@ import java.util.ArrayList;
 
 public class Extract {
 
-  int numPlaylists; //this might be irrelevant
-  Statement statement;
+  private int numPlaylists; //this might be irrelevant
+  private Statement statement;
+  private ArrayList<String> generatedPlaylists;
 
   public void setNumPlaylists() throws Exception {
     numPlaylists = statement.executeQuery("SELECT COUNT(*) FROM playlists;").getInt(1);
@@ -14,6 +15,10 @@ public class Extract {
 
   public int getNumPlaylists() {
     return numPlaylists;
+  }
+
+  public ArrayList<String> getGeneratedPlaylists() {
+    return generatedPlaylists;
   }
 
   public void fileHandler(String path) throws Exception { 
@@ -66,12 +71,11 @@ public class Extract {
   public void createPlaylist(int uid) throws Exception {
 
     ArrayList<String> videoURLS = queryVideoIDs(uid);
-    // System.out.println(videoURLS.size());
     int playlistsToCreate = (videoURLS.size() / 50) + 1;
     ArrayList<ArrayList <String>> subLists = new ArrayList<>();
-    ArrayList<String> generatedPlaylists = new ArrayList<>();
+    generatedPlaylists = new ArrayList<>();
 
-    for (int i = 0; i < playlistsToCreate; i++) { //make sublists
+    for (int i = 0; i < playlistsToCreate; i++) { //make sublists, youtube now has a maximum playlist size of 50
       if (videoURLS.size() < 50) {
         subLists.add(new ArrayList<String>(videoURLS.subList(0, videoURLS.size())));
         videoURLS.subList(0, videoURLS.size()).clear();;
@@ -80,7 +84,6 @@ public class Extract {
         videoURLS.subList(0, 49).clear();;
       }
       generatedPlaylists.add(playlistStringBuilder(subLists.get(i)));
-      // System.out.println(generatedPlaylists.get(i));
     }
   }
 
